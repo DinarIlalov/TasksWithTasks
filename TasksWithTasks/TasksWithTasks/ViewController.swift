10f74b9fc9f664836115594e28f080add106c79b
Dinar <dinarilalov@mail.ru>
2019-03-21 16:42:04 +0300

    init

---
+++ TasksWithTasks/TasksWithTasks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TasksWithTasks
+//
+//  Created by Dinar Ilalov on 21/03/2019.
+//  Copyright © 2019 Dinar Ilalov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
